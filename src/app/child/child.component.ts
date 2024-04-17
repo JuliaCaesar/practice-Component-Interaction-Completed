bf7d79388da244b21ca7daac1657623516392ec5
Pratap Kotala <pk@yahoo.com>
2024-04-17 09:56:02 -0500

    Completed project to demo input and output binding for component interaction

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-childcomponent',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.css'
+})
+export class ChildComponent {
+  @Input() inputText: string = "";
+  @Output() outputEvent: EventEmitter<string> = new EventEmitter<string>();
+
+  message:string = "";
+
+  sendMessage(){
+    this.outputEvent.emit(this.message);
+  }
+
+}
